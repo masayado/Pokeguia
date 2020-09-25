18b814a9c75213ecbdc702fb8cb196bb4e5cfff2
masayado <masayado@uc.cl>
2020-09-25 13:53:35 -0300

    inicie proyecto en vue con componente pokeguia

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    {{message}}
+    <poke-guia></poke-guia>
+  </div>
+</template>
+
+<script>
+  import Pokeguia from "./components/Pokeguia.vue";
+  export default {
+    data() {
+      return {
+        message: 'Hola en Vue',
+      };
+    },
+    components:{
+      'poke-guia': Pokeguia,
+    }
+  }
+</script>
+
+<style scoped>
+  #app {
+    font-size: 25px;
+    font-family: "Calibri", sans-serif;
+  }
+</style>
---
+++ src/components/Pokeguia.vue
@@ -0,0 +1,22 @@
+<template>
+    <input type="text" placeholder="Busca a tu pokémon por nombre">
+</template>
+
+<script>
+export default {
+    name: 'pokeguia-component',
+    // props: {},
+    data: function(){
+        return {}
+    },
+    // computed: {},
+    methods: {
+        // -- Metodos
+    },
+    //components: {},
+}
+</script>
+
+<style scoped>
+    
+</style>
